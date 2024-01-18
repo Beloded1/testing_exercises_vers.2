8c08f0d4131aaeb0db059daa02368de0dbb225da
Beloded1 <alexandrbeloded@gmail.com>
2024-01-18 16:16:49 +0400

    Done all at  level one except bank parser

--- tests/level_1/test_five_title.py
+++ tests/level_1/test_five_title.py
@@ -1,5 +1,17 @@
 from functions.level_1.five_title import change_copy_item
 
 
-def test_change_copy_item():
-    pass
+def test_change_copy_item_behavior_for_long_title():
+    assert change_copy_item('It is a very very long title', 20) == 'It is a very very long title'
+
+
+def test_change_copy_item_add_copy():
+    assert change_copy_item('Short title', 20) == 'Copy of Short title'
+
+
+def test_change_copy_item_increase_copy():
+    assert change_copy_item('Copy of my item (2)') == 'Copy of my item (3)'
+
+
+def test_change_copy_for_basic_text():
+    assert change_copy_item('Copy of my item') == 'Copy of my item (2)'
