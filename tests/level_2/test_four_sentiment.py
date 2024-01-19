c25adc991e20e42d667a35402e11fc697afa3411
Beloded1 <alexandrbeloded@gmail.com>
2024-01-19 14:00:00 +0400

    Finished one more task on second level

--- tests/level_2/test_four_sentiment.py
+++ tests/level_2/test_four_sentiment.py
@@ -7,12 +7,21 @@ def test__check_tweet_sentiment__if_not_good_and_not_bad_words():
 def test__check_tweet_sentiment__if_good_and_bad_words_are_equal():
     assert check_tweet_sentiment('This is a nicy and scary string', ['nicy', 'cool'], ['scary', 'ugly']) == None
 
-def test__check_tweet_sentiment__if_more_good_words():
+def test__check_tweet_sentiment__if_more_good_words_found():
     assert check_tweet_sentiment('This is a nicy and cool but scary string', ['nicy', 'cool'], ['scary', 'ugly']) == 'GOOD'
 
 
-def test__check_tweet_sentiment__if_more_bad_words():
+def test__check_tweet_sentiment__if_more_bad_words_found():
     assert check_tweet_sentiment('This is a nicy but scary and ugly string', ['nicy', 'cool'], ['scary', 'ugly']) == 'BAD'
+
+
+def test__check_tweet_sentiment__if_only_good_words_found():
+    assert check_tweet_sentiment('This is a nicy and cool string', ['nicy', 'cool'], ['scary', 'ugly']) == 'GOOD'
+
+
+def test__check_tweet_sentiment__if_only_bad_words_found():
+    assert check_tweet_sentiment('This is a scary and ugly string', ['nicy', 'cool'], ['scary', 'ugly']) == 'BAD'
+
 
 
 def test__check_tweet_sentiment__if_invalid_input():
