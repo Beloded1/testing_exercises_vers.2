4acdb3985c191fedbc606a8a119e0ac96584b054
Beloded1 <alexandrbeloded@gmail.com>
2024-01-29 15:01:39 +0400

    Done first task from level_3

--- functions/level_4/one_brackets.py
+++ functions/level_4/one_brackets.py
@@ -2,3 +2,6 @@ def delete_remove_brackets_quotes(name: str) -> str:
     if name[0] == '{':
         return name[2:len(name) - 2]
     return name
+
+
+print(delete_remove_brackets_quotes('{ Иван }'))
