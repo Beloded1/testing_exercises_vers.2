c3a416d4eecf8b7980d6d8eb9107ea64b505d9b9
Beloded1 <alexandrbeloded@gmail.com>
2024-01-30 14:37:16 +0400

    Done three_promocodes

--- functions/level_4/three_promocodes.py
+++ functions/level_4/three_promocodes.py
@@ -4,3 +4,7 @@ import string
 
 def generate_promocode(promocode_len: int = 8) -> str:
     return ''.join(random.choice(string.ascii_uppercase) for _ in range(promocode_len))
+
+# print(generate_promocode())
+# print(generate_promocode(1))
+
